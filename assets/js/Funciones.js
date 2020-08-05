a05b61585f83c48c0a7a12343ed65b26e37d8014
david1297 <juandavid.andrade1997@gmail.com>
2020-08-05 15:20:58 -0500

    .

---
+++ assets/js/Funciones.js
@@ -0,0 +1,41 @@
+function getCookie(cname) {
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for(var i = 0; i < ca.length; i++) {
+	  var c = ca[i];
+	  while (c.charAt(0) == ' ') {
+		c = c.substring(1);
+	  }
+	  if (c.indexOf(name) == 0) {
+		return c.substring(name.length, c.length);
+	  }
+	}
+	return "";
+  }
+
+  function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+ 
+ 
+ $(document).ready(function() {
+    if(getCookie('Carrito')==''){
+        var Carrito=makeid(20);
+        var d = new Date();
+        d.setTime(d.getTime() + (10*24*60*60*1000));
+        var expires = "expires="+ d.toUTCString();
+        document.cookie =  "Carrito="+Carrito+";" + expires + ";path=/";
+    }
+    
+    
+
+ 
+
+});
